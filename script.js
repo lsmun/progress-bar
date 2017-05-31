fd6004f6702bad876d3d0d2dc58dbaf454c3e047
Laura <laurasumun@gmail.com>
2017-05-31 10:42:41 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+class Matrix {
+	constructor(x,y) {
+		this.matrix = new Array();
+
+		for(var i = 0; i < x; i++) {
+			this.matrix.push(new Array());
+			for(var j = 0; j < y; j++) {
+				this.matrix[i].push(false);
+			}
+		}
+	}
+
+	get() {
+		return this.matrix;
+	}
+
+	state(x,y) {
+		return this.matrix[x][y];
+	}
+
+	turnOn(x,y) {
+		this.matrix[x][y] = true;
+	}
+
+	turnOff(x,y) {
+		this.matrix[x][y] = false;
+	}
+
+	toString() {
+		for(var i = 0; i < this.matrix.length; i++) {
+			for(var j = 0; j < this.matrix[i].length; j++) {
+				if(this.matrix[i][j] === true)
+					document.body.innerHTML += "*";
+				else
+					document.body.innerHTML += "0";
+			}
+			document.body.innerHTML += "<br>";
+		}
+	}
+}
+
+// Testing
+var sampleMatrix = new Matrix(5,5);
+sampleMatrix.turnOn(2,2);
+sampleMatrix.turnOn(2,3);
+sampleMatrix.turnOff(2,3);
+sampleMatrix.toString();
+
+console.log(sampleMatrix.get());
+console.log(sampleMatrix.state(2,2));
