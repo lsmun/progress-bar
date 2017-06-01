6152f48365adb68617e74c080f44b912e4fba6bf
Callum Ellard <cellard2014@outlook.com>
2017-06-01 16:09:07 -0400

    Fully functioning progress bar

--- public/script.js
+++ public/script.js
@@ -1,23 +1,34 @@
+
 class Matrix {
+
+  
+
+
 	constructor(x,y) {
 		this.matrix = new Array();
-
+        
 		for(var i = 0; i < x; i++) {
+            
 			this.matrix.push(new Array());
+            
 			for(var j = 0; j < y; j++) {
+                
 				this.matrix[i].push(false);
 			}
 		}
         
         
+        
         for(var i = 0; i < this.matrix.length; i++) {
 			for(var j = 0; j < this.matrix[i].length; j++) {
 					document.body.innerHTML += '<input type="checkbox" id="'+i+'-'+j+'">';
-                
+                    
                     
 			}
 			document.body.innerHTML += "<br>";
-	}
+             
+        }
+        
         
     }
     
@@ -28,6 +39,8 @@ class Matrix {
 	state(x,y) {
 		return this.matrix[x][y];
 	}
+    
+    
 
 	turnOn(x,y) {
 		this.matrix[x][y] = true;
@@ -37,18 +50,81 @@ class Matrix {
 	turnOff(x,y) {
 		this.matrix[x][y] = false;
         document.getElementById(x+'-'+y).checked = false;
+        
 	}
-
     
+    
+    timedLoop(){
+        
+        
+        var x = 0;
+        var myMatrix = this.matrix;
+        var rows = this.matrix.length;
+        var columns = this.matrix[0].length;
+        
+        
+        setInterval(function(){   
+            for(var m = 0; m < rows; m++) {
+                
+            
+                if(myMatrix[m][x] == true){
+                    
+                    
+                    myMatrix[m][x] = false;
+                    document.getElementById(m+'-'+x).checked = false;
+                }
+                else if(myMatrix[m][x] == false){
+                    
+                    
+                    myMatrix[m][x] = true;
+                    document.getElementById(m+'-'+x).checked = true;
+                }
+            }
+            
+            
+            if( x == columns - 1){
+                x = 0;
+            }
+            else{
+                x += 1;
+            }
+        } ,50);
+        
+            
+            
+    }
+    
+    
+    
+
+
 }
 
 
 // Testing
-var sampleMatrix = new Matrix(5,5);
-sampleMatrix.turnOn(2,2);
-sampleMatrix.turnOn(2,3);
-sampleMatrix.turnOff(2,3);
-sampleMatrix.toString();
 
-console.log(sampleMatrix.get());
-console.log(sampleMatrix.state(2,2));
+
+
+var sampleMatrix = new Matrix(5,35);
+
+sampleMatrix.timedLoop();
+
+
+
+
+    
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
