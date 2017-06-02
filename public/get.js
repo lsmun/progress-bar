69e73badfb3ecd8a9427d3a879df742a663a95ba
Callum Ellard <cellard2014@outlook.com>
2017-06-02 17:25:41 -0400

    deleted files

--- public/get.js
+++
@@ -1,8 +0,0 @@
-function getInfo(theURL){
-    
-    document.getElementById("data").action = theURL;
-}
-
-
-var theURL = 'https://api.darksky.net/forecast/0123456789abcdef9876543210fedcba/42.3601,-71.0589'
-getInfo(theURL);
